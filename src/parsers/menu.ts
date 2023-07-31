21711066b72b37df18f1911e0ed0dd736ffa85cf
rafamagalhas <magalhas.rafa@gmail.com>
2023-07-31 17:18:01 -0300

    fix: menu compose strategy

--- src/parsers/menu.ts
+++ src/parsers/menu.ts
@@ -1,15 +1,23 @@
-import { defaults } from 'lodash'
-
 export function menuTreeCompose(data: any[]) {
-  const nodes: any = {}
-  return data.filter(obj => {
-    const id = obj['_id'],
-      relatedId = obj['relatedId']
-
-    nodes[id] = defaults(obj, nodes[id], { submenus: [] })
-
-    relatedId &&
-      (nodes[relatedId] = nodes[relatedId] || { submenus: [] })['submenus'].push(obj)
-    return !relatedId
+  const transformedMenu = transformMenu(data);
+  const newMenu: any[] = [];
+  
+  transformedMenu.map((menu) => {
+    const subMenu = transformedMenu.filter((menuItem: { relatedId: String; }) => menuItem.relatedId === menu.id)
+    menu.subMenus = subMenu
+  
+    if (!menu.relatedId){
+      newMenu.push(menu);
+    }
   })
+  return newMenu;
+}
+
+function transformMenu(data: any[]) {
+  const transformedMenu = data.map(({_id, ...menuItems}) => ({
+    id: _id,
+    ...menuItems
+  }));
+
+  return transformedMenu;
 }
